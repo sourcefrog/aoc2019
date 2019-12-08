f9d98a50afdc8a9c5acf5bec31987c1cc5691854
Martin Pool <mbp@sourcefrog.net>
2019-12-07 21:42:29 -0800

    Add the other 03a examples

--- src/bin/aoc03.rs
+++ src/bin/aoc03.rs
@@ -122,6 +122,21 @@ mod test {
         let w1 = parse_path("R8,U5,L5,D3");
         let w2 = parse_path("U7,R6,D4,L4");
         assert_eq!(intersect_any(&w1, &w2), Some(6));
+
+        assert_eq!(
+            closest_intersection_from_strings(
+                "R75,D30,R83,U83,L12,D49,R71,U7,L72
+                U62,R66,U55,R34,D71,R55,D58,R83"
+            ),
+            Some(159)
+        );
+        assert_eq!(
+            closest_intersection_from_strings(
+                "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+                U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+            ),
+            Some(135)
+        );
     }
 
     #[test]
