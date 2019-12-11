5fb3b8ef5229c6a5ca04847b99bae4bcd3eb68e5
Martin Pool <mbp@sourcefrog.net>
2019-12-10 21:58:32 -0800

    Solve 11b

--- src/bin/aoc11.rs
+++ src/bin/aoc11.rs
@@ -4,12 +4,33 @@ use mbp_aoc2019::intcode::Computer;
 
 pub fn main() {
     println!("{}", solve_a());
+    solve_b();
 }
 
 fn solve_a() -> usize {
     // contains squares that have been painted, and their color.
     // false or unset is black; true is white.
-    let mut painted = BTreeMap::<(isize, isize), bool>::new();
+    let mut painted = BTreeMap::new();
+    paint_it(&mut painted);
+    painted.len()
+}
+
+fn solve_b() {
+    let mut painted = Map::new();
+    painted.insert((0, 0), true);
+    paint_it(&mut painted);
+    for y in -10..10 {
+        for x in -20..40 {
+            let p = painted.get(&(x, y)).copied().unwrap_or_default();
+            print!("{}", if p { "*" } else { " " });
+        }
+        println!();
+    }
+}
+
+type Map = BTreeMap<(isize, isize), bool>;
+
+fn paint_it(painted: &mut Map) {
     let mut pos = (0, 0);
     // 0=up, 1=left, etc.
     let mut dir = 0;
@@ -18,14 +39,14 @@ fn solve_a() -> usize {
         c.clear_input(); // in case it wasn't read
         c.push_input(painted.get(&pos).cloned().unwrap_or_default().into());
         if let Some(paint_this) = c.run_until_output() {
-            println!("paint {}", paint_this);
+            // println!("paint {}", paint_this);
             assert!(paint_this == 0 || paint_this == 1);
             painted.insert(pos, paint_this != 0);
         } else {
             break;
         }
         if let Some(turn) = c.run_until_output() {
-            println!("turn {}", turn);
+            // println!("turn {}", turn);
             dir = match turn {
                 0 => (dir + 3) % 4,
                 1 => (dir + 1) % 4,
@@ -42,6 +63,4 @@ fn solve_a() -> usize {
             break;
         }
     }
-    // The number of unique squares painted, even if repeatedly.
-    painted.len()
 }
