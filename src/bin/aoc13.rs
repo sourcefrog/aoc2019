330b4a7ed5e4972c49e80f702fed25d210c9c604
Martin Pool <mbp@sourcefrog.net>
2019-12-13 07:12:29 -0800

    Draw the map

--- src/bin/aoc13.rs
+++ src/bin/aoc13.rs
@@ -4,6 +4,7 @@ use mbp_aoc2019::intcode::Computer;
 
 pub fn main() {
     println!("13a: {}", solve_a());
+    println!("13b: {}", solve_b());
 }
 
 fn solve_a() -> usize {
@@ -16,13 +17,61 @@ fn solve_a() -> usize {
     for chunk in o.chunks(3) {
         if let [x, y, tile_id] = chunk {
             // dbg!(&x, &y, &tile_id);
-            map.insert((x, y), tile_id);
+            map.insert((*x, *y), *tile_id);
         } else {
             panic!("bad chunk? {:?}", chunk);
         }
     }
 
-    map.values().filter(|t| ***t == 2).count()
+    let map = Map { m: map };
+    println!("{}", map.draw());
+
+    map.m.values().filter(|t| **t == 2).count()
+}
+
+fn solve_b() -> usize {
+    let mut c = Computer::from_file("input/input13.txt");
+    0
+}
+
+struct Map {
+    m: BTreeMap<(isize, isize), isize>,
+}
+
+impl Map {
+    fn draw(&self) -> String {
+        let mut s = String::new();
+        let ((xmin,xmax),(ymin,ymax)) = self.bounds();
+        for y in ymin..=ymax {
+            for x in xmin..=xmax {
+                if let Some(t) = self.m.get(&(x, y)) {
+                    s.push(match t {
+                        0 => ' ',
+                        1 => '#',
+                        2 => 'a',
+                        3 => '_',
+                        4 => '0',
+                        _ => panic!(),
+                    });
+                }
+            }
+            s.push('\n');
+        }
+        s
+    }
+
+    fn bounds(&self) -> ((isize, isize), (isize, isize)) {
+        (
+            (
+                self.m.keys().map(|i| i.0).min().unwrap(),
+                 self.m.keys().map(|i| i.0).max().unwrap(),
+            ),
+            (
+                 self.m.keys().map(|i| i.1).min().unwrap(),
+                 self.m.keys().map(|i| i.1).max().unwrap(),
+            ),
+        )
+    }
 }
 
 #[cfg(test)]
