19d48146461ee786dcc88179a4350ec6992af3a6
Martin Pool <mbp@sourcefrog.net>
2019-12-15 10:42:31 -0800

    Start on 13b
    Start to play the game, although not yet choosing the right input
    or playing all the way through.

--- src/bin/aoc13.rs
+++ src/bin/aoc13.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, BTreeSet};
 
 use mbp_aoc2019::intcode::Computer;
 
@@ -8,47 +8,85 @@ pub fn main() {
 }
 
 fn solve_a() -> usize {
-    let mut c = Computer::from_file("input/input13.txt");
-    c.run();
-    let o = c.drain_output();
-    assert!(o.len() % 3 == 0);
+    let mut g = Game::load();
+    g.dry_run();
+    g.m.values().filter(|t| **t == 2).count()
+}
 
-    let mut map = BTreeMap::new();
-    for chunk in o.chunks(3) {
-        if let [x, y, tile_id] = chunk {
-            // dbg!(&x, &y, &tile_id);
-            map.insert((*x, *y), *tile_id);
-        } else {
-            panic!("bad chunk? {:?}", chunk);
+fn solve_b() -> isize {
+    Game::load().play()
+}
+
+struct Game {
+    c: Computer,
+    m: BTreeMap<(isize, isize), isize>,
+    score: isize,
+
+    // Positions of remaining blocks
+    blocks: BTreeSet<Point>,
+    // Position of paddle
+    paddle: Option<Point>,
+}
+
+type Point = (isize, isize);
+
+const BLOCK: isize = 2;
+const PADDLE: isize = 3;
+
+impl Game {
+    fn load() -> Game {
+        Game {
+            c: Computer::from_file("input/input13.txt"),
+            m: BTreeMap::new(),
+            score: -1,
+            blocks: BTreeSet::new(),
+            paddle: None,
         }
     }
 
-    let map = Map { m: map };
-    println!("{}", map.draw());
+    fn dry_run(&mut self) {
+        self.c.run();
+        self.consume_output();
+        println!("{}", self.draw());
+    }
 
-    map.m.values().filter(|t| **t == 2).count()
-}
+    fn consume_output(&mut self) {
+        for chunk in self.c.drain_output().chunks(3) {
+            if let [x, y, t] = chunk {
+                let x = *x;
+                let y = *y;
+                let t = *t;
+                // dbg!(&x, &y, &tile_id);
+                if x == -1 && y == 0 {
+                    self.score = t;
+                } else {
+                    self.m.insert((x, y), t);
+                    if t == PADDLE {
+                        self.paddle = Some((x, y))
+                    };
+                    if t == BLOCK {
+                        self.blocks.insert((x, y));
+                    } else {
+                        self.blocks.remove(&(x, y));
+                    }
+                }
+            } else {
+                panic!("bad chunk? {:?}", chunk);
+            }
+        }
+    }
 
-fn solve_b() -> usize {
-    let mut c = Computer::from_file("input/input13.txt");
-    0
-}
-
-struct Map {
-    m: BTreeMap<(isize, isize), isize>,
-}
-
-impl Map {
     fn draw(&self) -> String {
         let mut s = String::new();
-        let ((xmin,xmax),(ymin,ymax)) = self.bounds();
+        s.push_str(&format!("Score: {}\n", self.score));
+        let ((xmin, xmax), (ymin, ymax)) = self.bounds();
         for y in ymin..=ymax {
             for x in xmin..=xmax {
                 if let Some(t) = self.m.get(&(x, y)) {
                     s.push(match t {
                         0 => ' ',
                         1 => '#',
-                        2 => 'a',
+                        2 => '.',
                         3 => '_',
                         4 => '0',
                         _ => panic!(),
@@ -64,13 +102,40 @@ impl Map {
         (
             (
                 self.m.keys().map(|i| i.0).min().unwrap(),
-                 self.m.keys().map(|i| i.0).max().unwrap(),
+                self.m.keys().map(|i| i.0).max().unwrap(),
             ),
             (
-                 self.m.keys().map(|i| i.1).min().unwrap(),
-                 self.m.keys().map(|i| i.1).max().unwrap(),
+                self.m.keys().map(|i| i.1).min().unwrap(),
+                self.m.keys().map(|i| i.1).max().unwrap(),
             ),
         )
+    }
+
+    // To play the game: the only thing we control is the position of the
+    // paddle, and it seems like all we can do is put that in the position
+    // the ball will next come down to the bottom line.
+    //
+    // The field is wide enough that it looks like we can't just wait until
+    // the ball is moving down and then move the paddle, because it's much
+    // wider than it is deep...
+    //
+    // Although actually, rather than simulating the game, I wonder if
+    // we can just peek and work out what value will be output?
+    //
+    // The (probably too obvious) guess would be it's one point per block...
+    // But it's not.
+    //
+    // Q: Do we need to predict where the ball will bounce, projecting it through
+    // all collisions? I wonder how predictable it is...
+    //
+    // Q: It seems we don't know which direction the ball is initally travelling.
+    //
+    // This seems to also actually require teaching the computer to run until it
+    // wants input and then stop.
+    fn play(&mut self) -> isize {
+        self.c.poke_at(0, 2);
+        while !self.blocks.is_empty() {}
+        self.score
     }
 }
 
