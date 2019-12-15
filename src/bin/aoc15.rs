3abffcd335c7bf943d74f9f6f6234bf9d9c86348
Martin Pool <mbp@sourcefrog.net>
2019-12-15 15:22:09 -0800

    Solve 15b

--- src/bin/aoc15.rs
+++ src/bin/aoc15.rs
@@ -1,11 +1,13 @@
 #![allow(dead_code, unused_mut, unused_variables)]
 
+use std::collections::btree_map::Entry;
 use std::collections::BTreeMap;
 
 use mbp_aoc2019::intcode::Computer;
 
 pub fn main() {
     println!("15a: {}", solve_a());
+    println!("15b: {}", solve_b());
 }
 
 #[derive(Eq, PartialEq, Clone, Debug, Copy)]
@@ -26,7 +28,7 @@ impl Square {
         }
     }
 
-    fn to_char(&self) -> char {
+    fn to_char(self) -> char {
         match self {
             Empty => '.',
             Wall => 'x',
@@ -71,93 +73,139 @@ impl Move {
 type Point = (isize, isize);
 
 fn solve_a() -> usize {
-    // Naively, we'd want to do a breadth-first search from the starting point
-    // until we find the oxygen.
-    //
-    // This is slightly complicated by not being able to jump to any arbitrary
-    // point - except, we could store the computer state and jump there.
-    //
-    // We need to simultaneously discover the contents of the squares,
-    // which squares are reachable, and also the length of the shorest path
-    // to get there...
-    //
-    // By looking at all squares N positions away, before looking any deeper,
-    // we should find the shortest path first.
-    let orig_c = Computer::from_file("input/input15.txt");
-
-    // Contents of all the squares we've seen.
-    let mut seen: BTreeMap<Point, Square> = BTreeMap::new();
-
-    // Squares we want to visit, preceded by their depth, and including
-    // the computer state when in them.
-    let mut queue: Vec<(usize, Point, Computer)> = Vec::new();
-    queue.push((0, (0, 0), orig_c));
-
-    loop {
-        queue.sort_by_key(|a| a.0);
-        println!("queue len {}", queue.len());
-        let (depth, oldpos, cpu) = queue.remove(0);
-
-        // Try to visit every square neighboring `oldpos`, starting from `cpu`.
-        for m in Move::all_moves() {
-            let newpos = m.move_from(&oldpos);
-            if seen.contains_key(&newpos) {
-                // println!("have already seen {:?}, skipping", &newpos);
-                continue;
-            }
-
-            let mut newcpu = cpu.clone();
-            newcpu.push_input(m.command());
-            newcpu.run();
-            assert_eq!(newcpu.output_len(), 1);
-            let sq = Square::from_output(newcpu.pop_output().unwrap());
-            // println!(".. result: {:?}", sq);
-
-            println!(
-                "move {:?} from {:?} to {:?} at depth {} => {:?}",
-                m, oldpos, newpos, depth, sq
-            );
-            match sq {
-                Oxygen => {
-                    println!("found oxygen at {:?}", newpos);
-                    return depth + 1;
-                }
-                Wall => {}
-                Empty => {
-                    let newdepth = depth + 1;
-                    queue.push((newdepth, newpos, newcpu));
-                }
-            }
-
-            seen.insert(newpos, sq);
-        }
-
-        println!("{}", draw(&seen, &oldpos));
-    }
+    Map::load().oxygen_depth
 }
 
-fn draw(map: &BTreeMap<Point, Square>, pos: &Point) -> String {
-    if map.is_empty() {
-        return String::new();
-    }
-    let xmin = map.keys().map(|p| p.0).min().unwrap();
-    let xmax = map.keys().map(|p| p.0).max().unwrap();
-    let ymin = map.keys().map(|p| p.1).min().unwrap();
-    let ymax = map.keys().map(|p| p.1).max().unwrap();
-    let mut s = String::new();
-    for y in ymin..=ymax {
-        for x in xmin..=xmax {
-            if *pos == (x, y) {
-                s.push('@')
-            } else if let Some(sq) = map.get(&(x, y)) {
-                s.push(sq.to_char());
-            } else {
-                s.push(' ')
+fn solve_b() -> usize {
+    Map::load().flood_oxygen()
+}
+
+struct Map {
+    seen: BTreeMap<Point, Square>,
+    oxygen_depth: usize,
+    oxygen_pos: Point,
+}
+
+impl Map {
+    fn load() -> Map {
+        // Naively, we'd want to do a breadth-first search from the starting point
+        // until we find the oxygen.
+        //
+        // This is slightly complicated by not being able to jump to any arbitrary
+        // point - except, we could store the computer state and jump there.
+        //
+        // We need to simultaneously discover the contents of the squares,
+        // which squares are reachable, and also the length of the shorest path
+        // to get there...
+        //
+        // By looking at all squares N positions away, before looking any deeper,
+        // we should find the shortest path first.
+        let orig_c = Computer::from_file("input/input15.txt");
+
+        // Contents of all the squares we've seen.
+        let mut seen: BTreeMap<Point, Square> = BTreeMap::new();
+
+        // Squares we want to visit, preceded by their depth, and including
+        // the computer state when in them.
+        let mut queue: Vec<(usize, Point, Computer)> = Vec::new();
+        queue.push((0, (0, 0), orig_c));
+
+        let mut oxygen_depth = None;
+        let mut oxygen_pos = None;
+
+        while !queue.is_empty() {
+            queue.sort_by_key(|a| a.0);
+            // println!("queue len {}", queue.len());
+            let (depth, oldpos, cpu) = queue.remove(0);
+
+            // Try to visit every square neighboring `oldpos`, starting from `cpu`.
+            for m in Move::all_moves() {
+                let newpos = m.move_from(&oldpos);
+                if seen.contains_key(&newpos) {
+                    // println!("have already seen {:?}, skipping", &newpos);
+                    continue;
+                }
+
+                let mut newcpu = cpu.clone();
+                newcpu.push_input(m.command());
+                newcpu.run();
+                assert_eq!(newcpu.output_len(), 1);
+                let sq = Square::from_output(newcpu.pop_output().unwrap());
+                // println!(".. result: {:?}", sq);
+
+                // println!( "move {:?} from {:?} to {:?} at depth {} => {:?}", m, oldpos, newpos, depth, sq);
+                match sq {
+                    Oxygen => {
+                        // println!("found oxygen at {:?}", newpos);
+                        oxygen_depth = Some(depth + 1);
+                        oxygen_pos = Some(newpos);
+                    }
+                    Wall => {}
+                    Empty => {
+                        let newdepth = depth + 1;
+                        queue.push((newdepth, newpos, newcpu));
+                    }
+                }
+
+                seen.insert(newpos, sq);
             }
         }
-        s.push('\n');
+        let map = Map {
+            seen,
+            oxygen_depth: oxygen_depth.unwrap(),
+            oxygen_pos: oxygen_pos.unwrap(),
+        };
+        println!("{}", map.draw(&(0, 0)));
+        map
     }
-    s
+
+    fn draw(&self, pos: &Point) -> String {
+        if self.seen.is_empty() {
+            return String::new();
+        }
+        let xmin = self.seen.keys().map(|p| p.0).min().unwrap();
+        let xmax = self.seen.keys().map(|p| p.0).max().unwrap();
+        let ymin = self.seen.keys().map(|p| p.1).min().unwrap();
+        let ymax = self.seen.keys().map(|p| p.1).max().unwrap();
+        let mut s = String::new();
+        for y in ymin..=ymax {
+            for x in xmin..=xmax {
+                if *pos == (x, y) {
+                    s.push('@')
+                } else if let Some(sq) = self.seen.get(&(x, y)) {
+                    s.push(sq.to_char());
+                } else {
+                    s.push(' ')
+                }
+            }
+            s.push('\n');
+        }
+        s
+    }
+
+    fn flood_oxygen(&mut self) -> usize {
+        // Locations of as-yet-incomplete oxygen.
+        let mut oxy = vec![self.oxygen_pos];
+        for i in 0.. {
+            let mut new_oxy = Vec::new();
+            for oxypos in oxy.into_iter() {
+                for m in Move::all_moves() {
+                    let qxy = m.move_from(&oxypos);
+                    if let Entry::Occupied(mut o) = self.seen.entry(qxy) {
+                        if *o.get() == Empty {
+                            new_oxy.push(qxy);
+                            o.insert(Oxygen);
+                        }
+                    }
+                }
+            }
+            if new_oxy.is_empty() {
+                return i;
+            }
+            oxy = new_oxy;
+        }
+        unreachable!()
+    }
 }
 
 #[cfg(test)]
@@ -176,5 +224,10 @@ mod test {
     #[test]
     fn solution_a() {
         assert_eq!(solve_a(), 232);
+    }
+
+    #[test]
+    fn solution_b() {
+        assert_eq!(solve_b(), 320);
     }
 }
