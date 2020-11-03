852712a72a7ff5b120766f036609e0a83956aecb
Martin Pool <mbp@sourcefrog.net>
2020-11-03 06:08:39 -0800

    Finally solve 16b!

--- src/bin/aoc16.rs
+++ src/bin/aoc16.rs
@@ -4,13 +4,55 @@ use std::convert::TryInto;
 
 const REPEATS: usize = 10_000;
 const PHASES: usize = 100;
+const OFFSET_LEN: usize = 7;
+const OUTPUT_LEN: usize = 8;
 
 pub fn main() {
-    //    println!(
-    //        "{:?}",
-    //    );
-    println!("{:?}", solve_a());
-    println!("{:?}", solve_b());
+    println!("{}", solve_a());
+    println!("{}", solve_b());
+}
+
+fn solve_b() -> String {
+    let input = &std::fs::read_to_string("input/input16.txt").unwrap();
+    solve_type_b(&input)
+}
+
+fn solve_type_b(input: &str) -> String {
+    let s = str_to_vi8(input);
+    let offset = input[..OFFSET_LEN].parse().unwrap();
+    let total_len = s.len() * REPEATS;
+
+    // For offsets in this range, every coefficient to the left of `offset`
+    // is 0, and everything to the right is 1. Therefore in each phase,
+    // for all i in offset..=total_len, new_a[i] is just the sum of
+    // old_a[i..]. We never need to look at anything left of offset.
+    assert!(offset < total_len && offset * 2 > total_len);
+
+    // Calculate the expanded vector, omitting everything left of offset.
+    let mut a: Vec<i8> = (offset..total_len).map(|i| s[i % s.len()]).collect();
+    // show_vec(&a);
+
+    let ol = total_len - offset;
+    debug_assert_eq!(ol, a.len());
+
+    for _phase in 0..PHASES {
+        let mut last = 0;
+        for i in (0..ol).rev() {
+            last = (last + a[i]) % 10;
+            a[i] = last;
+        }
+        // show_vec(&a);
+    }
+
+    vi8_to_str(&a[..OUTPUT_LEN])
+}
+
+fn show_vec(a: &[i8]) {
+    println!(
+        "{} ... {}",
+        vi8_to_str(&a[..30]),
+        vi8_to_str(&a[(a.len() - 30)..])
+    )
 }
 
 fn vi8_to_str(a: &[i8]) -> String {
@@ -55,62 +97,6 @@ fn solve_a() -> String {
     )
 }
 
-fn solve_b() -> String {
-    let instr = &std::fs::read_to_string("input/input16.txt").unwrap();
-    solve_type_b(&instr)
-}
-
-fn solve_type_b(s: &str) -> String {
-    let _a = str_to_vi8(s);
-    let _offset: usize = s[..7].parse().unwrap();
-
-    // Calculating everything by brute force over 100M * 100 might be a bit slow...
-    //
-    // Digit j of the output is the sum of roughly 1/4th of the input digits, minus
-    // the sum of 1/4 of the others. These occur at intervals that are predictable
-    // from j.
-    //
-    // It's the sum of blocks of length (j+1), repeating on a cycle of (j+1)*4,
-    // starting at position j. And minus same-size blocks half out of phase.
-    //
-    // So that cuts out a significant bit but we still probably look at all the
-    // input...
-    //
-    // All this is done modulo 10 which seems to perhaps offer some more scope to
-    // avoid work...
-    //
-    // So the FFT is the sum of products of the inputs. If the input is repeated
-    // 10k times, that just means the multipliers are larger.
-    //
-    // I think to calculate output position i (at least in the first cycle)
-    // we can find FFT(a)[i] = sum_j(M(i, j) * a[j])
-    // and M() can be defined in closed form.
-    //
-    // For 1 repeat, M is just an expanded cycle of [0, 1, 0, -1] but if the
-    // list is to be repeated, then more generally we need to calculate how many
-    // times a[j] will occur at positions wtih both additive, and negative,
-    // effects, respectively.
-    //
-    // We could define FFT(a * n)[i] = sum_j(MR(i, j, len(a), r) * a[j]).
-    //
-    // The counts in MR seem to come from the divmod relationship of len(a), r,
-    // j, and i... Which seems a little complicated but probably possible to
-    // compute in closed form.
-    //
-    // But then calculating 100 phases of this seems harder, because there's
-    // no guarantee the intermediate input repeats; in fact it seems unlikely
-    // to. But, computing a ~6.5M element vector where each element depends on
-    // 6.5M inputs is infeasible, and seems like it shouldn't really be
-    // necessary. .
-    //
-    // Could it be possible to compute MRP(i, j, len(a), r, p) which is the
-    // multiplier for a[j] to calculate output digit i when a is repeated r
-    // times, and the whole thing is run for p phases? It might be possible.
-    // It seems like the result would still have this form of a sum of
-    // products of the original input, a.
-    unimplemented!();
-}
-
 fn fftn(mut a: Vec<i8>, n: usize) -> Vec<i8> {
     for _ in 0..n {
         a = fft1(&a);
@@ -128,5 +114,26 @@ mod test {
         assert_eq!(fft1(&str_to_vi8("12345678")), str_to_vi8("48226158"));
 
         assert_eq!(fftn(str_to_vi8("12345678"), 4), str_to_vi8("01029498"));
+    }
+
+    #[test]
+    fn examples_b() {
+        assert_eq!(
+            &solve_type_b("03036732577212944063491565474664"),
+            "84462026"
+        );
+        assert_eq!(
+            &solve_type_b("02935109699940807407585447034323"),
+            "78725270"
+        );
+        assert_eq!(
+            &solve_type_b("03081770884921959731165446850517"),
+            "53553731"
+        );
+    }
+
+    #[test]
+    fn expected_answer_b() {
+        assert_eq!(&solve_b(), "96099551");
     }
 }
