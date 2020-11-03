4ef80dfd1ef5278d5988b377e28b16f5320a5a9a
Martin Pool <mbp@sourcefrog.net>
2020-11-03 10:27:28 -0800

    Simplify 19b solution

--- src/bin/aoc19.rs
+++ src/bin/aoc19.rs
@@ -3,7 +3,7 @@ use std::cmp::max;
 use mbp_aoc2019::intcode::Computer;
 
 pub fn main() {
-    // println!("19a: {}", solve_a());
+    println!("19a: {}", solve_a());
     println!("19b: {}", solve_b());
 }
 
@@ -17,15 +17,25 @@ fn solve_a() -> usize {
     for y in 0..50 {
         for x in 0..50 {
             if is_lit(&program, x, y) {
-                print!("#");
                 tracted += 1;
+            }
+        }
+    }
+    tracted
+}
+
+#[allow(dead_code)]
+fn show_map(program: &Computer) {
+    for y in 0..50 {
+        for x in 0..50 {
+            if is_lit(&program, x, y) {
+                print!("#");
             } else {
                 print!(".");
             }
         }
         println!();
     }
-    tracted
 }
 
 fn solve_b() -> isize {
@@ -40,24 +50,60 @@ const SQUARE: isize = 100;
 /// Bisect through rows, finding the size of the square starting on that row
 /// that fits in the beam, until we find the smallest adequate answer.
 fn solve_type_b(program: &Computer) -> isize {
-    let mut lower_bound = 1;
-    let mut higher_bound = 10_000;
-    let mut best_corner = None;
-    while lower_bound + 1 < higher_bound {
-        let guess = (lower_bound + higher_bound) / 2;
-        if let Some((x, y)) = large_square(program, guess) {
-            best_corner = Some((x, y));
-            higher_bound = guess
-        } else {
-            lower_bound = guess
+    let mut min_first = 0;
+    let mut min_last = 0;
+    for y in 0.. {
+        // dbg!(y);
+        // Is anything lit on this row, and if so where does it start?
+        if let Some(top_first) = first_lit(program, min_first, y) {
+            let top_last = last_lit(program, max(top_first, min_last), y);
+            min_first = top_first; // No need to look left of this on later rows
+            min_last = top_last;
+            let square_x = top_last - SQUARE + 1;
+            if square_x < top_first {
+                // Not even enough room on this row
+                continue;
+            }
+            // Now look down from here to see if there is a full square.
+            if is_lit(program, square_x, y + SQUARE - 1) {
+                // dbg!(square_x, y);
+                debug_assert!(is_lit(program, square_x + SQUARE - 1, y));
+                debug_assert!(is_lit(program, square_x + SQUARE - 1, y + SQUARE - 1));
+                return square_x * 10_000 + y;
+            }
         }
     }
-    let (x, y) = best_corner.unwrap();
-    x * 10_000 + y
+    unreachable!()
+}
+
+/// Return the x coordinate of the first lit cell in row Y, at at least min_x, if any.
+fn first_lit(program: &Computer, min_x: isize, y: isize) -> Option<isize> {
+    // Some rows have nothing lit, so we have a kinda kludgey assumption that we will find
+    // nothing too far to the right of the unit slope.
+    for x in min_x..max(10, 2 * y) {
+        if is_lit(program, x, y) {
+            return Some(x);
+        }
+    }
+    None
+}
+
+/// Find the x coordinate of the last lit cell in row Y, guaranteed to be at least
+/// min_x, and where min_x is guaranteed to be lit.
+fn last_lit(program: &Computer, min_x: isize, y: isize) -> isize {
+    debug_assert!(is_lit(program, min_x, y), "{}, {} is not lit", min_x, y);
+    for x in (min_x + 1).. {
+        if !is_lit(program, x, y) {
+            debug_assert!(x > min_x);
+            return x - 1;
+        }
+    }
+    unreachable!()
 }
 
 /// True if the cell at (x, y) is lit.
 fn is_lit(program: &Computer, x: isize, y: isize) -> bool {
+    // println!("is_lit ({}, {})", x, y);
     let mut cpu = program.clone();
     assert!(x >= 0);
     assert!(y >= 0);
@@ -69,59 +115,6 @@ fn is_lit(program: &Computer, x: isize, y: isize) -> bool {
         1 => true,
         0 => false,
         other => panic!("unexpected output {:?}", other),
-    }
-}
-
-/// Calculate the inclusive range of lit squares on row Y.
-// TODO: Memoize these calculations; they're likely to reoccur.
-fn lit_range(program: &Computer, y: isize, min_x: isize) -> Option<(isize, isize)> {
-    let mut first_lit = None;
-    let mut last_lit = None;
-    // Some rows have nothing lit, so we have a kinda kludgey assumption that we will find
-    // nothing too far to the right of the unit slope.
-    for x in min_x..max(10, 2 * y) {
-        if is_lit(program, x, y) {
-            if first_lit.is_none() {
-                first_lit = Some(x)
-            }
-            last_lit = Some(x)
-        } else {
-            if first_lit.is_some() {
-                break;
-            }
-        }
-    }
-    match (first_lit, last_lit) {
-        (Some(first), Some(last)) => Some((first, last)),
-        (None, None) => None,
-        _ => panic!(),
-    }
-}
-
-/// Find a full-size square starting on row y, if there is one, and return its top-left corner coordinate.
-fn large_square(program: &Computer, y: isize) -> Option<(isize, isize)> {
-    if let Some((top_left, top_right)) = lit_range(program, y, 0) {
-        if top_right - top_left + 1 < SQUARE {
-            None
-        } else {
-            // Look down SQUARE rows, and find the range there.
-            if let Some((bottom_left, bottom_right)) = lit_range(program, y + SQUARE - 1, top_left)
-            {
-                // They both have some lit squares. Are there at least 'size' columns in common?
-                // This all assumes the beam slopes down to the right.
-                debug_assert!(bottom_left >= top_left);
-                debug_assert!(bottom_right >= top_right);
-                if top_right - bottom_left + 1 >= SQUARE {
-                    Some((bottom_left, y))
-                } else {
-                    None
-                }
-            } else {
-                None
-            }
-        }
-    } else {
-        None
     }
 }
 
@@ -140,17 +133,6 @@ mod test {
         assert_eq!(is_lit(&program, 0, 0), true);
         assert_eq!(is_lit(&program, 0, 1), false);
         assert_eq!(is_lit(&program, 0, 10), false);
-    }
-
-    /// Check lit ranges from the early part of the map against visually
-    /// inspected values.
-    #[test]
-    fn lit_ranges() {
-        let program = load_program();
-        assert_eq!(lit_range(&program, 0, 0), Some((0, 0)));
-        assert_eq!(lit_range(&program, 9, 4), Some((8, 8)));
-        assert_eq!(lit_range(&program, 10, 2), Some((8, 9)));
-        assert_eq!(lit_range(&program, 40, 0), Some((32, 36)));
     }
 
     #[test]
